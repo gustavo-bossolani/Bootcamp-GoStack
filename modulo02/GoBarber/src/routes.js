7f2f92901cedded3cb5253ab3df6d61a5431256e
gustavo.bossolani <gustavo.bossolani@resource.com.br>
2019-12-10 15:20:51 -0300

    model e inserção de users

--- modulo02/GoBarber/src/routes.js
+++ modulo02/GoBarber/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get('/', (req, resp) => resp.json({ mensagem: 'Olá World!' }));
+let teste = 0;
+routes.get('/', async (req, resp) => {
+
+    const user = await User.create({
+        name: 'teste',
+        email: 'teste@email.com',
+        password_hash: '1312',
+    });
+    return resp.json(user);
+});
 
 export default routes;
