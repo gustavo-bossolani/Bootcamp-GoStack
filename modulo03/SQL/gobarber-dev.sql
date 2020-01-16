08a65443e6eb6c27876aae31d11333c46c9bd20d
gustabo-bossolani <gustavo.wyd@outlook.com>
2020-01-16 18:02:23 -0300

    workspace insomnia gobarber.sql

--- modulo03/SQL/gobarber-dev.sql
+++ modulo03/SQL/gobarber-dev.sql
@@ -8,6 +8,11 @@ select * from files;
 select * from appointments;
 
 
+/*---------- JOIN AREA ----------*/
+/*---------- USER & FILES ----------*/
+select users.name as user_name, files.name as file_name 
+from users join files where users.avatar_id = files.id;
+
 /*---------- TRUNCATE AREA ----------*/
 truncate users;
 truncate files;
