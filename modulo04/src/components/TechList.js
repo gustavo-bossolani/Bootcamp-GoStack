76e789d41fcbf7d29e6a0eb3dbdfccdbecd96856
gustabo-bossolani <gustavo.wyd@outlook.com>
2020-02-04 14:11:21 -0300

    conceitos de React

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -9,13 +9,32 @@ import TechItem from './TechItem';
 class TechList extends Component {
     state = {
         newTech: '',
-        techs: [
-            'Git-Gud',
-            'Gud-Git',
-            'Git',
-            'Gud'
-        ]
+        techs: []
     };
+
+    // Executado assim que o componente aparece em tela
+    componentDidMount() {
+        const techs = localStorage.getItem('techs');
+
+        if(techs) {
+            this.setState({ techs: JSON.parse(techs) });
+        }
+    }
+
+    // Executado sempre que houver alterações nas props ou estado
+    componentDidUpdate(_, prevState) {
+        // prevProps referencia aos antigos props antes da mudança 
+        // prevState referencia aos antigos states antes da mudança 
+        // É possível acessar os novos props ou states através do this.porps e this.state
+        if(prevState.techs != this.state.techs){
+            localStorage.setItem('techs', JSON.stringify(this.state.techs));
+        }
+    }
+
+    // Executado quando o componente deixa de existir
+    componentWillUnmount() {
+        
+    }
 
     handleInputChange = e => {
         this.setState({ newTech: e.target.value });
@@ -24,7 +43,6 @@ class TechList extends Component {
     handleSubmit = e => {
         e.preventDefault();
         this.setState({ techs: [...this.state.techs, this.state.newTech], newTech: '' });
-
     }
 
     handleDelete = tech => {
@@ -39,17 +57,17 @@ class TechList extends Component {
                 <h2>{this.state.newTech}</h2>
                 <form onSubmit={this.handleSubmit}>
                     <ul>
+                        <input 
+                            type="text"
+                            onChange={this.handleInputChange}
+                            value={this.state.newTech} />
+                        <button type="submit">Enviar</button>
                         { this.state.techs.map(tech => 
                             <TechItem  
                                 key={tech} 
                                 tech={tech} 
                                 onDelete={() => this.handleDelete(tech)} />)}
                     </ul>
-                    <input 
-                        type="text"
-                        onChange={this.handleInputChange}
-                        value={this.state.newTech} />
-                    <button type="submit">Enviar</button>
                 </form>
             </>
         )
